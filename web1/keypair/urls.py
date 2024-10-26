8e790339fb5bba3fc67b1babcc23a65bc5e17264
Sihuong06 <11212553@st.neu.edu.vn>
2024-10-26 23:30:08 +0700

    abaabab

--- web1/keypair/urls.py
+++ web1/keypair/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('expire-keypair/<str:verification_code>/', views.expire_keypair, name='expire_keypair'),
     path('generate-keypair/<str:verification_code>/', views.generate_keypair, name='generate_keypair'),
 ]
+
